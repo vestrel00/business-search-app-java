060ffc9d5887f5fc5f5973be022f98f4ee9f77da
Vandolf Estrellado <vestrel00@gmail.com>
2017-04-26 14:01:20 -0400

    Added base fragment and load data view

---
+++ presentation-android-mvp/src/main/java/com/vestrel00/business/search/presentation/android/mvp/ui/common/BaseLoadDataFragment.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2017 Vandolf Estrellado
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.vestrel00.business.search.presentation.android.mvp.ui.common;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.vestrel00.business.search.presentation.android.mvp.R;
+
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * An abstract {@link BaseFragment} that implements {@link LoadDataView}.
+ * <p>
+ * This fragment assumes that the following views with the given ids exist in the view returned in
+ * {@link #onCreateView(LayoutInflater, ViewGroup, Bundle)}:
+ * <ul>
+ * <li>R.id.loading_indicator : the loading indicator view</li>
+ * <li>R.id.retry_button : the retry button view</li>
+ * </ul>
+ * This uses a {@link Toast} for showing errors via {@link #showError(String)}.
+ */
+public abstract class BaseLoadDataFragment extends BaseFragment implements LoadDataView {
+
+    @Inject
+    protected Context context;
+
+    @BindView(R.id.loading_indicator)
+    protected View loadingIndicator;
+
+    @BindView(R.id.retry_button)
+    protected View retryButton;
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ButterKnife.bind(this, view);
+    }
+
+    @Override
+    public void showLoading() {
+        loadingIndicator.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideLoading() {
+        loadingIndicator.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showRetry() {
+        retryButton.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideRetry() {
+        retryButton.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void showError(String message) {
+        Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+    }
+}
