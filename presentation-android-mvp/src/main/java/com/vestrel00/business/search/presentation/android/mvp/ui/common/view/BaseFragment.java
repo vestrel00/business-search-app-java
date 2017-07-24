b0efe10238d5279af74075f984ef83514dc8afde
Vandolf Estrellado <vestrel00@gmail.com>
2017-07-24 12:06:47 -0400

    Added Nullable annotation to viewUnbinder in BaseFragment

--- presentation-android-mvp/src/main/java/com/vestrel00/business/search/presentation/android/mvp/ui/common/view/BaseFragment.java
+++ presentation-android-mvp/src/main/java/com/vestrel00/business/search/presentation/android/mvp/ui/common/view/BaseFragment.java
@@ -20,6 +20,7 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
@@ -60,6 +61,7 @@ public abstract class BaseFragment extends Fragment implements HasFragmentInject
     @Inject
     DispatchingAndroidInjector<Fragment> childFragmentInjector;
 
+    @Nullable
     private Unbinder viewUnbinder;
 
     @Override
